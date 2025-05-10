6be5e5f57f5fe95fce511d34f8e686b2f82b227a
SnowCoder404 <contact@marco-lenschau.de>
2025-05-10 03:27:15 +0200

    Add add-task button

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import { FullCalendarModule } from '@fullcalendar/angular';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-calendar',
-  imports: [FullCalendarModule],
+  imports: [FullCalendarModule, MatIconModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent {
+  events = [];
+  
   calendarOptions = {
     plugins: [dayGridPlugin],
     initialView: 'dayGridMonth',
-    events: [
-      { title: 'Feedback', date: '2025-05-10', color: 'green' },
-      { title: 'Meeting', date: '2025-05-11', color: 'red' }
-    ]
+    events: this.events
   };
 }
