ed43a337811cbf504fbdadd1e271eecd6b4d2f8d
Marco Lenschau <contact@marco-lenschau.de>
2025-05-22 07:37:41 +0200

    Add serach user funktion

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -19,12 +19,48 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 })
 export class UserComponent {
   items$: Observable<any[]>;
-  
+  users: any = []
+
   constructor(public dialog: MatDialog, private firestore: FirebaseServices) {
     this.items$ = this.firestore.getColRef("users");
+    this.loadBackendData();
   }
 
   addUser() {
     this.dialog.open(DialogAddUserComponent);
+  }
+
+  searchUser(event: Event) {
+    const searchUser = (event.target as HTMLInputElement).value;
+    const searchUserArray: any = []
+    if (searchUser.length > 2) {
+      this.users.filter((user: any) => {
+        if (user.firstName.toLowerCase().includes(searchUser)) {
+          searchUserArray.push(user);
+        }
+      })
+      this.users = searchUserArray;
+    } else {
+      this.loadBackendData();
+    }
+
+  }
+
+  loadBackendData() {
+    this.items$.forEach((users)=> {
+      this.users = users;
+    });
+  }
+
+  sortUser(checkbox: boolean, filter: string) {
+    if (checkbox) {
+      this.users.sort((a: any, b: any) => {
+        if (!a[filter]) return 1;
+        if (!b[filter]) return -1;
+        return a[filter].localeCompare(b[filter]);
+      });
+    } else {
+      this.loadBackendData();
+    }
   }
 }
